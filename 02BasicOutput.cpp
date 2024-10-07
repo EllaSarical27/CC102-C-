42d4e5b2618aeef64da5fd59ad69f3eb7c9de9e6
ellayy <saricalella27@gmail.com>
2024-10-07 08:40:03 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation";
+	cout << "College of Computer Studies";
+	cout << "BSIT-1C";
+	cout << "Ella B. Sarical";
+	cout << "Purok 5 Barangay Napilihan Vinzons Camarines Norte";
+	
+ return 0;
+}
