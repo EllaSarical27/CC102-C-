f78e99653601dd82be4c6858122a24e5875dd2cf
ellayy <saricalella27@gmail.com>
2024-10-07 08:41:36 +0800

    Add files via upload

---
+++ 04Basicinput.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string nickname,course,year,school;
+	
+	cout << "Enter Your Nickname\t\t\t:";
+	cin >> nickname;
+	cout << "Enter Your Course\t\t:";
+	cin >> course;
+	cout << "Enter your Year:";
+	cin >> year;
+	cout << "Enter Your School\t\t\t:";
+	cin >> school;
+	
+	cout << "Wow Congrats" << nickname << "!" << "which is one of the Center of Excellence in Tertiary Education."; 
+	return 0;
+}
